f118fed80368ef046c1ed0c27c9109e9815400c9
brian-dizon <140157607+brian-dizon@users.noreply.github.com>
2026-03-02 11:40:38 +0800

    01 - wp, next js, apollo-graphql setup, init shadCN

---
+++ lib/apollo-client.ts
@@ -0,0 +1,17 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const rawEndpoint =
+  process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "http://localhost:10073/graphql";
+
+const GRAPHQL_ENDPOINT = rawEndpoint.endsWith("/graphql")
+  ? rawEndpoint
+  : `${rawEndpoint.replace(/\/$/, "")}/graphql`;
+
+export const getClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: GRAPHQL_ENDPOINT,
+    }),
+    cache: new InMemoryCache(),
+  });
+};
